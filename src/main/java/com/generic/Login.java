0d6f0bec58efc682fc587ee5c29711f50d9be11f
mdhaque <qamhaue777@gmail.com>
2023-11-18 18:10:30 -0500

    first commit

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,52 @@
+package com.generic;
+
+public class Login {
+	
+//	static int mysalary = 5000;
+//	static String myname = "Sarowar";
+//	static double price =50.50;
+//	
+//	
+//	Login obj = new Login();
+//	Login obj1 = new Login();
+//	Login obj2 = new Login();
+//	
+//	
+//	
+//	public static void main(String[] args) {
+//
+//		System.out.println("Hello All");
+//		
+//		System.out.println(mysalary);
+//		System.out.println(price);
+//		System.out.println(myname);
+//		
+//	}
+	
+	
+	int a = 20; //  variable name a , global variable 
+	
+	public static void main(String[] args) {
+		
+//		int 	a= 10;
+//		double  b = 12.20;	
+//		boolean	c = true;
+//		String  d = "Sarowsr";
+//		
+//		// int + letter = two ways 
+//		
+//		String e ="Sarowar";
+//		 Object  f ="sarowar123@";
+//		 var   g ="sarowar@123";	
+		
+		
+		int a =20; // variable a local variable
+		
+		
+	}
+	
+	
+	
+	
+
+}
